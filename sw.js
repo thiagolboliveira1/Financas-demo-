a8fbfcc4784bffa2781bfa5584c888755103d3e5
Thiago Oliveira <thiagolboliveira@hotmail.com>
2025-11-08 15:44:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch',e=>{});
+
+const CACHE = 'flow-v7-3';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/logo.png?v=7.3'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
