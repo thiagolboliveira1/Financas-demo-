f204e1169b4dc254c977a73c52a494a8ce60dba7
Thiago Oliveira <thiagolboliveira@hotmail.com>
2025-11-08 11:35:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('nublue-v3').then(cache=>cache.addAll([
-    './','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', (e)=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
+self.addEventListener('fetch',e=>{});
